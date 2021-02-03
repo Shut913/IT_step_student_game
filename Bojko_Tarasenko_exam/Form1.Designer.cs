edbc53a1f9524276433fa85c01a51f6fddb4a034
Tarasenko Sergey <shut913@gmail.com>
2021-02-03 20:10:16 +0200

    start proj here

---
+++ Bojko_Tarasenko_exam/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace Bojko_Tarasenko_exam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectPnl = new System.Windows.Forms.Panel();
+            this.locationPnl = new System.Windows.Forms.Panel();
+            this.timePnl = new System.Windows.Forms.Panel();
+            this.characteristicsPnl = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // selectPnl
+            // 
+            this.selectPnl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.selectPnl.Location = new System.Drawing.Point(0, 525);
+            this.selectPnl.Name = "selectPnl";
+            this.selectPnl.Size = new System.Drawing.Size(1004, 133);
+            this.selectPnl.TabIndex = 0;
+            // 
+            // locationPnl
+            // 
+            this.locationPnl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.locationPnl.Location = new System.Drawing.Point(0, 0);
+            this.locationPnl.Name = "locationPnl";
+            this.locationPnl.Size = new System.Drawing.Size(815, 525);
+            this.locationPnl.TabIndex = 1;
+            // 
+            // timePnl
+            // 
+            this.timePnl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.timePnl.Location = new System.Drawing.Point(815, 0);
+            this.timePnl.Name = "timePnl";
+            this.timePnl.Size = new System.Drawing.Size(189, 525);
+            this.timePnl.TabIndex = 2;
+            // 
+            // characteristicsPnl
+            // 
+            this.characteristicsPnl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.characteristicsPnl.Location = new System.Drawing.Point(815, 76);
+            this.characteristicsPnl.Name = "characteristicsPnl";
+            this.characteristicsPnl.Size = new System.Drawing.Size(189, 449);
+            this.characteristicsPnl.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 658);
+            this.Controls.Add(this.characteristicsPnl);
+            this.Controls.Add(this.timePnl);
+            this.Controls.Add(this.locationPnl);
+            this.Controls.Add(this.selectPnl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel selectPnl;
+        private System.Windows.Forms.Panel locationPnl;
+        private System.Windows.Forms.Panel timePnl;
+        private System.Windows.Forms.Panel characteristicsPnl;
+    }
+}
+
---
+++ Bojko_Tarasenko_exam/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Bojko_Tarasenko_exam")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bojko_Tarasenko_exam")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d0fc5a97-7cdd-4560-bf81-506266b035bd")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
